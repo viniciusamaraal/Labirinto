8833dfd75745b5e1fe1f51d3b271bd0bc3b6538c
Vinícius Amaral <viniciusluiz.doamaral@gmail.com>
2016-10-04 20:05:47 -0300

    Incluindo no controle de versão.

---
+++ Labirinto/TAD/Labirinto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirinto.TAD
+{
+    public class Labirinto
+    {
+        Posicao[,] Mapa { get; set; }
+
+        public Labirinto(int nLinhas, int nColunas)
+        {
+            Mapa = new Posicao[nLinhas, nColunas];
+        }
+
+        public void IncluirPosicao(int x, int y, char tipoCampo)
+        {
+            Mapa[x, y] = new Posicao(tipoCampo);
+        }
+
+        public void ImprimirMapa()
+        {
+            for (int i = 0; i < Mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < Mapa.GetLength(1); j++)
+                    Console.Write(Mapa[i, j].tipo);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Labirinto/TAD/Posicao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirinto.TAD
+{
+    public class Posicao
+    {
+        public char tipo { get; set; }
+        public bool visitado { get; set; }
+
+        public Posicao(char tipoEntrada)
+        {
+            tipo = tipoEntrada;
+        }
+    }
+}
